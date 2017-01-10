d4023bb01bed59d98e61876216a346b289aea43f
marvinh <marvinharootoonyan@gmail.com>
2017-01-09 20:16:07 -0800

    unlock web audio ios

--- scripts/Synth.js
+++ scripts/Synth.js
@@ -7,6 +7,21 @@ osc = new Sine();
 aenv = new DecayEnv();
 penv = new PitchEnv();
 
+window.addEventListener('touchstart', function() {
+
+	// create empty buffer
+	var buffer = myContext.createBuffer(1, 1, 22050);
+	var source = myContext.createBufferSource();
+	source.buffer = buffer;
+
+	// connect to output (your speakers)
+	source.connect(myContext.destination);
+
+	// play the file
+	source.noteOn(0);
+
+}, false);
+
 
 var audioContext = new AudioContext();
 source = audioContext.createBufferSource();

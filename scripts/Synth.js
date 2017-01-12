11df08292d390ca938f2f2bad1edabe617010241
marvinh <marvinharootoonyan@gmail.com>
2017-01-11 23:07:46 -0800

    initialize button

--- scripts/Synth.js
+++ scripts/Synth.js
@@ -14,6 +14,7 @@ source = audioContext.createBufferSource();
 var scriptNode = audioContext.createScriptProcessor(4096, 0, 1);
 var kickButton = document.getElementById('kickButton');
 var initButton = document.getElementById('initButton');
+initButton.addEventListener('touchstart', process_touchstart, false);
 osc.inc = 60.0/audioContext.sampleRate;
 aenv.sampleRate = audioContext.sampleRate;
 penv.sampleRate = audioContext.sampleRate;
@@ -66,7 +67,20 @@ scriptNode.onaudioprocess = function(audioProcessingEvent) {
     }
   }
 }
-
+function process_touchstart(ev) {
+  // Use the event's data to call out to the appropriate gesture handlers
+  switch (ev.touches.length) {
+    case 1: handle_one_touch(ev);
+      console.log('init button fired');
+      source.connect(scriptNode);
+      scriptNode.connect(audioContext.destination);
+      source.start();
+      break;
+    case 2: handle_two_touches(ev); break;
+    case 3: handle_three_touches(ev); break;
+    default: gesture_not_supported(ev); break;
+  }
+}
 initButton.onclick = function() {
   console.log('init button fired');
   source.connect(scriptNode);
